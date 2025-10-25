ba51b732399b95bc3030635e2b6d9d74e46d60da
Ivan YT <ivanfabrizio395@gmail.com>
2025-10-25 00:24:18 +0000

    haz que este titulo sea mas epica y un poco mas grande  (_for element <H

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="flex h-16 items-center justify-between border-b bg-card shadow-sm px-4">
       <div className="w-8"></div>
-      <h1 className="text-xl font-bold tracking-tight text-foreground font-headline">
+      <h1 className="text-2xl font-bold tracking-tight text-foreground font-headline drop-shadow-md">
         Desafío HV
       </h1>
       <ThemeToggle />
